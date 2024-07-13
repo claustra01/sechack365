3bb3f1a56e9548aaf0bed6993a88eafbae3b4899
claustra01 <shinya430.g@gmail.com>
2024-07-14 02:33:25 +0900

    add: mock nodeinfo response

--- api/pkg/handler/wellknown.go
+++ api/pkg/handler/wellknown.go
@@ -1,18 +1,76 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
-	"log/slog"
 	"net/http"
 
 	"github.com/claustra01/sechack365/pkg/framework"
 )
 
+type nodeinfo struct {
+	OpenRegistrations bool     `json:"openRegistrations"`
+	Protocols         []string `json:"protocols"`
+	Software          software `json:"software"`
+	Usage             usage    `json:"usage"`
+	Services          services `json:"services"`
+	Metadata          metadata `json:"metadata"`
+	Version           string   `json:"version"`
+}
+
+type software struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type usage struct {
+	Users usersUsage `json:"users"`
+}
+
+type usersUsage struct {
+	Total int `json:"total"`
+}
+
+type services struct {
+	Inbound  struct{} `json:"inbound"`
+	Outbound struct{} `json:"outbound"`
+}
+
+type metadata struct{}
+
 func Nodeinfo(c *framework.Context) http.HandlerFunc {
+
+	// Mock nodeinfo
+	nodeinfo := nodeinfo{
+		OpenRegistrations: false,
+		Protocols: []string{
+			"activitypub",
+		},
+		Software: software{
+			Name:    "sechack365",
+			Version: "0.1.0",
+		},
+		Usage: usage{
+			Users: usersUsage{
+				Total: 1,
+			},
+		},
+		Services: services{
+			Inbound:  struct{}{},
+			Outbound: struct{}{},
+		},
+		Metadata: metadata{},
+		Version:  "2.1",
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("Host:", "host", c.Config.Host)
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, `{"version":"0.1.0"}`)
+		data, err := json.Marshal(nodeinfo)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			panic(err)
+		}
+		fmt.Fprint(w, string(data))
 	}
 }
 
