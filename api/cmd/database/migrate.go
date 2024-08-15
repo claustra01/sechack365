63ec7defe621b6bed70ba58b52f8c3cbbb2f520d
claustra01 <shinya430.g@gmail.com>
2024-08-16 01:05:28 +0900

    fix: migration

--- api/cmd/database/migrate.go
+++ api/cmd/database/migrate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/claustra01/sechack365/pkg/database"
@@ -16,12 +17,27 @@ func main() {
 }
 
 func migrate(conn *database.SqlHandler) {
-	conn.Pg.Exec(`CREATE TABLE IF NOT EXISTS users (
-		id VARCHAR(255),
-		username VARCHAR(255) NOT NULL,
-		encrypted_password VARCHAR(255) NOT NULL,
-		display_name VARCHAR(255) NOT NULL,
-		profile VARCHAR(255) NOT NULL,
-		PRIMARY KEY (id)
-	);`)
+	_, err := conn.Pg.Exec(`
+		DROP TABLE IF EXISTS users;
+		DROP TABLE IF EXISTS ap_identity;
+		CREATE TABLE users (
+			id VARCHAR(255),
+			user_id VARCHAR(255) NOT NULL,
+			encrypted_password VARCHAR(255) NOT NULL,
+			display_name VARCHAR(255) NOT NULL,
+			profile VARCHAR(255) NOT NULL,
+			PRIMARY KEY (id)
+		);
+		CREATE TABLE ap_identity (
+			user_id VARCHAR(255) NOT NULL,
+			public_key VARCHAR(255) NOT NULL,
+			secret_key VARCHAR(255) NOT NULL,
+			PRIMARY KEY (user_id),
+			FOREIGN KEY (user_id) REFERENCES users(id)
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("migration success")
 }
