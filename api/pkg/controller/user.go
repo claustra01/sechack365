689cfb6aecfa895c8023e6e6ae8c5c7ba82d9a24
claustra01 <shinya430.g@gmail.com>
2024-08-21 01:35:41 +0900

    add: user controller

---
+++ api/pkg/controller/user.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"github.com/claustra01/sechack365/pkg/model"
+	"github.com/claustra01/sechack365/pkg/repository"
+	"github.com/claustra01/sechack365/pkg/usecase"
+)
+
+type UserController struct {
+	UserUsecase usecase.UserUsecase
+}
+
+func NewUserController(conn model.ISqlHandler) *UserController {
+	return &UserController{
+		UserUsecase: usecase.UserUsecase{
+			UserRepository: &repository.UserRepository{
+				SqlHandler: conn,
+			},
+		},
+	}
+}
+
+func (controller *UserController) FindAll() (users []*model.User, err error) {
+	return controller.UserUsecase.FindAll()
+}
+
+func (controller *UserController) FindById(id string) (user *model.User, err error) {
+	return controller.UserUsecase.FindById(id)
+}
+
+func (controller *UserController) FindByUserId(userId string) (user *model.User, err error) {
+	return controller.UserUsecase.FindByUserId(userId)
+}
