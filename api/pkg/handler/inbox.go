e5733754d36720d9325019f64a830000a267d3a3
claustra01 <shinya430.g@gmail.com>
2024-08-30 16:05:17 +0900

    update: get signarure haeder in inbox

--- api/pkg/handler/inbox.go
+++ api/pkg/handler/inbox.go
@@ -11,12 +11,15 @@ import (
 func ActorInbox(c *framework.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		username := r.PathValue("username")
+		header := r.Header
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			returnInternalServerError(w, c.Logger, err)
 			return
 		}
-		log.Println(username, string(body))
+		log.Println(username)
+		log.Println(header.Get("Signature"))
+		log.Println(string(body))
 		returnInternalServerError(w, c.Logger, nil)
 	}
 }
