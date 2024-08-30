f40104825e8c11407a14223ab7293ff4035324bb
claustra01 <shinya430.g@gmail.com>
2024-08-30 15:09:26 +0900

    wip: add mock inbox and outbox

---
+++ api/pkg/handler/outbox.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/claustra01/sechack365/pkg/framework"
+)
+
+func ActorOutbox(c *framework.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username := r.PathValue("username")
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			returnInternalServerError(w, c.Logger, err)
+			return
+		}
+		log.Println(username, string(body))
+		returnInternalServerError(w, c.Logger, nil)
+	}
+}
