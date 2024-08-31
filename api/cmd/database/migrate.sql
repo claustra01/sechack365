9f82160a6832e8cd0c8b9f46b7397323f165feb7
claustra01 <shinya430.g@gmail.com>
2024-09-01 01:02:13 +0900

    update: db migration

--- api/cmd/database/migrate.sql
+++ api/cmd/database/migrate.sql
@@ -7,36 +7,36 @@ CREATE TABLE "users" (
   "id" varchar(255) PRIMARY KEY,
   "username" varchar(255) NOT NULL,
   "host" varchar(255) NOT NULL,
-  "protocol" protocol,
+  "protocol" protocol NOT NULL,
   "hashed_password" varchar(255),
   "display_name" varchar(255),
   "profile" text,
   "icon" varchar(255),
-  "created_at" timestamp DEFAULT 'CURRENT_TIMESTAMP',
-  "updated_at" timestamp DEFAULT 'CURRENT_TIMESTAMP'
+  "created_at" timestamp DEFAULT 'NOW()',
+  "updated_at" timestamp DEFAULT 'NOW()'
 );
 
 CREATE TABLE "ap_user_identifiers" (
-  "user_id" vatchar(255) PRIMARY KEY,
+  "user_id" varchar(255) PRIMARY KEY,
   "public_key" text NOT NULL,
   "private_key" text NOT NULL,
-  "created_at" timestamp DEFAULT 'CURRENT_TIMESTAMP',
-  "updated_at" timestamp DEFAULT 'CURRENT_TIMESTAMP'
+  "created_at" timestamp DEFAULT 'NOW()',
+  "updated_at" timestamp DEFAULT 'NOW()'
 );
 
 CREATE TABLE "assets" (
   "id" varchar(255) PRIMARY KEY,
   "url" varchar(255) NOT NULL,
-  "created_at" timestamp DEFAULT 'CURRENT_TIMESTAMP',
-  "updated_at" timestamp DEFAULT 'CURRENT_TIMESTAMP'
+  "created_at" timestamp DEFAULT 'NOW()',
+  "updated_at" timestamp DEFAULT 'NOW()'
 );
 
 CREATE TABLE "follows" (
   "id" varchar(255) PRIMARY KEY,
   "follower_id" varchar(255) NOT NULL,
   "followee_id" varchar(255) NOT NULL,
-  "created_at" timestamp DEFAULT 'CURRENT_TIMESTAMP',
-  "updated_at" timestamp DEFAULT 'CURRENT_TIMESTAMP'
+  "created_at" timestamp DEFAULT 'NOW()',
+  "updated_at" timestamp DEFAULT 'NOW()'
 );
 
 ALTER TABLE "users" ADD FOREIGN KEY ("icon") REFERENCES "assets" ("id");
