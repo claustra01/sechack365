79d41c8f35512956b99f05239899c3a36a48d48d
claustra01 <shinya430.g@gmail.com>
2024-09-01 20:52:08 +0900

    add: errors

--- api/pkg/cerror/error.go
+++ api/pkg/cerror/error.go
@@ -7,6 +7,10 @@ var ErrUserNotFound = fmt.Errorf("user not found")
 var ErrResolveWebfinger = fmt.Errorf("failed to resolve webfinger")
 var ErrResolveRemoteActor = fmt.Errorf("failed to resolve remote actor")
 
+var ErrInvalidFollowRequest = fmt.Errorf("invalid follow request")
+
+var ErrPushActivity = fmt.Errorf("failed to push activity")
+
 var ErrGeneratePasswordHash = fmt.Errorf("failed to generate password hash")
 var ErrGenerateRsaKey = fmt.Errorf("failed to generate RSA key pair")
 var ErrEncodePublicKey = fmt.Errorf("failed to encode public key")
@@ -14,6 +18,7 @@ var ErrEncodePrivateKey = fmt.Errorf("failed to encode private key")
 var ErrDecodePublicKey = fmt.Errorf("failed to decode public key")
 var ErrDecodePrivateKey = fmt.Errorf("failed to decode private key")
 var ErrInvalidKeyPair = fmt.Errorf("invalid key pair")
+var ErrGenerateSignature = fmt.Errorf("failed to generate signature")
 
 func ErrInvalidResourceQuery(q string) error {
 	return fmt.Errorf("invalid resource query: %s", q)
