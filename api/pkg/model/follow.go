c88df1cdc8a1b3a17359e9c992539ade4669eb58
claustra01 <shinya430.g@gmail.com>
2024-09-01 17:08:46 +0900

    update: follow model schema

--- api/pkg/model/follow.go
+++ api/pkg/model/follow.go
@@ -2,8 +2,8 @@ package model
 
 type Follow struct {
 	Id         string `json:"id"`
-	Follower   string `json:"follower"`
-	Followee   string `json:"followee"`
+	FollowerId string `json:"follower_id"`
+	FolloweeId string `json:"followee_id"`
 	IsAccepted bool   `json:"is_accepted"`
 	CreatedAt  string `json:"created_at"`
 	UpdatedAt  string `json:"updated_at"`
