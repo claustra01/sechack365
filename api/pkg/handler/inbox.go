2fd84cd729068b8f335813761852a7aef07ae228
claustra01 <shinya430.g@gmail.com>
2024-09-01 23:29:41 +0900

    update: get inbox digest header for debug

--- api/pkg/handler/inbox.go
+++ api/pkg/handler/inbox.go
@@ -12,13 +12,16 @@ func ActorInbox(c *framework.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		username := r.PathValue("username")
 		headers := r.Header
+		digest := headers.Get("Digest")
 		signature := headers.Get("Signature")
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			returnInternalServerError(w, c.Logger, err)
 			return
 		}
+		// FIXME: remove debug log
 		log.Println(username)
+		log.Println(digest)
 		log.Println(signature)
 		log.Println(string(body))
 		returnInternalServerError(w, c.Logger, nil)
