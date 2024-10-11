c76b4d44e5788cccb24affc2fc0edc33168118a8
claustra01 <shinya430.g@gmail.com>
2024-10-11 22:27:01 +0900

    add: generate mock endpoint

--- api/pkg/handler/dev.go
+++ api/pkg/handler/dev.go
@@ -8,8 +8,17 @@ import (
 
 func GenerateMock(c *framework.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		jsonResponse(w, map[string]interface{}{
-			"message": "Hello, Developer!",
-		})
+		users, err := c.Controllers.User.FindAll()
+		if err != nil {
+			panic(err)
+		}
+		if len(users) > 0 {
+			w.Write([]byte("Mock Data Already Exists"))
+			return
+		}
+		if _, err := c.Controllers.User.Create("mock", c.Config.Host, "local", "Mock User", "This is mock user", "https://placehold.jp/150x150.png"); err != nil {
+			panic(err)
+		}
+		w.Write([]byte("Mock Data Created"))
 	}
 }
