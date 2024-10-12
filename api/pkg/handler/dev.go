6cb31aeceb85f40eae65fd59329b1e740cea30de
claustra01 <shinya430.g@gmail.com>
2024-10-12 17:09:20 +0900

    add: reset mock data handler

--- api/pkg/handler/dev.go
+++ api/pkg/handler/dev.go
@@ -46,3 +46,31 @@ func GenerateMock(c *framework.Context) http.HandlerFunc {
 		}
 	}
 }
+
+func ResetMock(c *framework.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users, err := c.Controllers.User.FindAll()
+		if err != nil {
+			panic(err)
+		}
+		if len(users) == 0 {
+			if _, err := w.Write([]byte("Mock Data Not Found")); err != nil {
+				// NOTE: err should be nil
+				panic(err)
+			}
+			return
+		}
+		for _, user := range users {
+			if err := c.Controllers.ApUserIdentifier.DeleteById(user.Id); err != nil {
+				panic(err)
+			}
+			if err := c.Controllers.User.DeleteById(user.Id); err != nil {
+				panic(err)
+			}
+		}
+		if _, err := w.Write([]byte("Mock Data Deleted")); err != nil {
+			// NOTE: err should be nil
+			panic(err)
+		}
+	}
+}
