84cb51a9ef9452af97ff56bfde43e916fe859942
claustra01 <shinya430.g@gmail.com>
2024-11-12 15:31:21 +0900

    add: user follow/follower usecase

--- api/pkg/usecase/follow.go
+++ api/pkg/usecase/follow.go
@@ -5,6 +5,8 @@ import "github.com/claustra01/sechack365/pkg/model"
 type IFollowRepository interface {
 	Create(followerId, followeeId string) (*model.Follow, error)
 	UpdateAcceptFollow(followerId, followeeId string) (*model.Follow, error)
+	FindFollowsByUserId(userId string) ([]*model.User, error)
+	FindFollowersByUserId(userId string) ([]*model.User, error)
 }
 
 type FollowUsecase struct {
@@ -17,4 +19,12 @@ func (u *FollowUsecase) Create(followerId, followeeId string) (*model.Follow, er
 
 func (u *FollowUsecase) UpdateAcceptFollow(followerId, followeeId string) (*model.Follow, error) {
 	return u.FollowRepository.UpdateAcceptFollow(followerId, followeeId)
+}
+
+func (u *FollowUsecase) FindFollowsByUserId(userId string) ([]*model.User, error) {
+	return u.FollowRepository.FindFollowsByUserId(userId)
+}
+
+func (u *FollowUsecase) FindFollowersByUserId(userId string) ([]*model.User, error) {
+	return u.FollowRepository.FindFollowersByUserId(userId)
 }
