3f49765d872524cc0367ddd63e8e04784236eb85
claustra01 <shinya430.g@gmail.com>
2024-11-15 02:17:57 +0900

    chore: page template include main tag

--- frontend/src/components/Template/Template.tsx
+++ frontend/src/components/Template/Template.tsx
@@ -1,11 +1,11 @@
 import { getApiV1UsersMe } from "@/openapi";
 import { Box } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { UserMenu } from "../Menu/UserMenu";
 import { GuestMenu } from "../Menu/GuestMenu";
+import { UserMenu } from "../Menu/UserMenu";
 
 export const PageTemplate = ({ children }: { children: React.ReactNode }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
 	useEffect(() => {
 		getApiV1UsersMe().then((response) => {
@@ -13,16 +13,26 @@ export const PageTemplate = ({ children }: { children: React.ReactNode }) => {
 		});
 	}, []);
 
-  return (
-    <Box bg="#E7F5FF" style={{display: "flex", flexDirection: "column", alignItems: "center", minHeight: "100vh"}}>
-      <Box style={{display: "flex", gap: "24px"}}>
-        <Box bg="#FFF" w={720} style={{minHeight: "100vh"}}>
-          {children}
-        </Box>
-        <Box style={{marginTop: "92px"}}>
-          {isAuthenticated ? <UserMenu /> : <GuestMenu />}
-        </Box>
-      </Box>
-    </Box>
-  );
-}
+	return (
+		<main>
+			<Box
+				bg="#E7F5FF"
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					minHeight: "100vh",
+				}}
+			>
+				<Box style={{ display: "flex", gap: "24px" }}>
+					<Box bg="#FFF" w={720} style={{ minHeight: "100vh" }}>
+						{children}
+					</Box>
+					<Box style={{ marginTop: "92px" }}>
+						{isAuthenticated ? <UserMenu /> : <GuestMenu />}
+					</Box>
+				</Box>
+			</Box>
+		</main>
+	);
+};
