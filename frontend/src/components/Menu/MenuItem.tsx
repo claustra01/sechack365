8c26dff5012e22472de20cafb04d06e9230f23c4
claustra01 <shinya430.g@gmail.com>
2024-11-14 21:39:22 +0900

    fix: menuitem text

--- frontend/src/components/Menu/MenuItem.tsx
+++ frontend/src/components/Menu/MenuItem.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Box, Text } from "@mantine/core";
+import { ActionIcon, Box, Text, Title } from "@mantine/core";
 import Link from "next/link";
 
 export type MenuItemProps = {
@@ -13,7 +13,7 @@ export const MenuItem = (props: MenuItemProps) => {
       <ActionIcon component={Link} href={props.href} variant="subtle" size="xl" color="blue">
         {props.icon}
       </ActionIcon>
-      <Text size="xl" fw={500} c="blue">{props.title}</Text>
+      <Title size="h3" fw={500} c="blue">{props.title}</Title>
     </Box>
   )
 }
