1625985f428ee596dd91ba9db4d195efc31a3523
claustra01 <shinya430.g@gmail.com>
2024-11-15 00:55:42 +0900

    fix: lint

--- frontend/src/components/Timeline/GuestTimeline.tsx
+++ frontend/src/components/Timeline/GuestTimeline.tsx
@@ -1,26 +1,24 @@
 import { getApiV1Timeline } from "@/openapi";
-import { Post } from "@/openapi/schemas";
+import type { Post } from "@/openapi/schemas";
+import { Box } from "@mantine/core";
+import { IconHome } from "@tabler/icons-react";
 import { useState } from "react";
 import { Header } from "../Header/Header";
-import { IconHome } from "@tabler/icons-react";
-import { Box } from "@mantine/core";
 import { PostCard } from "../PostCard/PostCard";
 
 export const GuestTimeline = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+	const [posts, setPosts] = useState<Post[]>([]);
 
-  getApiV1Timeline().then((response) => {
-    setPosts(response.data as unknown as Post[]);
-  })
+	getApiV1Timeline().then((response) => {
+		setPosts(response.data as unknown as Post[]);
+	});
 
-  return(
-    <Box w={720}>
-      <Header title={"Home"} icon={<IconHome />} />
-      {posts.map((post) => {
-        return (
-          <PostCard key={post.id} {...post} />
-        )
-      })}
-    </Box>
-  )
-}
+	return (
+		<Box w={720}>
+			<Header title={"Home"} icon={<IconHome />} />
+			{posts.map((post) => {
+				return <PostCard key={post.id} {...post} />;
+			})}
+		</Box>
+	);
+};
