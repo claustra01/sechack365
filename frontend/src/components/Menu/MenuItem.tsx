d2533179cec15a516dfe60cf237c59a9c7b17f7b
claustra01 <shinya430.g@gmail.com>
2024-11-14 21:18:20 +0900

    add: menu item component

---
+++ frontend/src/components/Menu/MenuItem.tsx
@@ -0,0 +1,19 @@
+import { ActionIcon, Box, Text } from "@mantine/core";
+import Link from "next/link";
+
+export type MenuItemProps = {
+  icon: JSX.Element;
+  title: string;
+  href: string;
+}
+
+export const MenuItem = (props: MenuItemProps) => {
+  return (
+    <Box style={{ display: "flex", alignItems: "center" }}>
+      <ActionIcon component={Link} href={props.href} variant="subtle" size="xl" color="blue">
+        {props.icon}
+      </ActionIcon>
+      <Text size="xl" color="blue">{props.title}</Text>
+    </Box>
+  )
+}
