d78fdb7665712b352c0df83eac9fc562b689e8c9
claustra01 <shinya430.g@gmail.com>
2024-11-15 00:38:31 +0900

    fix: lint

--- frontend/src/components/Post/PostCard.tsx
+++ frontend/src/components/Post/PostCard.tsx
@@ -1,13 +1,21 @@
-import { Post } from "@/openapi/schemas"
-import { PostUserCard } from "./PostUserCard"
-import { Box, Text } from "@mantine/core"
+import type { Post } from "@/openapi/schemas";
+import { Box, Text } from "@mantine/core";
+import { PostUserCard } from "./PostUserCard";
 
 export const PostCard = (props: Post) => {
-  console.log(props)
-  return (
-    <Box style={{display: "flex", flexDirection: "column", padding: "24px", gap: "24px", borderTop: "2px solid #1C7ED6"}}>
-      <PostUserCard {...props.user} />
-      <Text size="lg">{props.content}</Text>
-    </Box>
-  )
-}
+	console.log(props);
+	return (
+		<Box
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				padding: "24px",
+				gap: "24px",
+				borderTop: "2px solid #1C7ED6",
+			}}
+		>
+			<PostUserCard {...props.user} />
+			<Text size="lg">{props.content}</Text>
+		</Box>
+	);
+};
