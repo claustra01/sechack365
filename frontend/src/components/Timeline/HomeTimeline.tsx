25cc0ffbb74da57eeb5d4aa894d1ba356260a754
claustra01 <shinya430.g@gmail.com>
2024-11-15 01:31:59 +0900

    add: home timeline component

---
+++ frontend/src/components/Timeline/HomeTimeline.tsx
@@ -0,0 +1,26 @@
+import { getApiV1Timeline } from "@/openapi";
+import type { Post } from "@/openapi/schemas";
+import { Box } from "@mantine/core";
+import { IconHome } from "@tabler/icons-react";
+import { useState } from "react";
+import { Header } from "../Header/Header";
+import { PostCard } from "../PostCard/PostCard";
+import { NewPost } from "../NewPost/NewPost";
+
+export const HomeTimeline = () => {
+	const [posts, setPosts] = useState<Post[]>([]);
+
+	getApiV1Timeline().then((response) => {
+		setPosts(response.data as unknown as Post[]);
+	});
+
+	return (
+		<Box w={720}>
+			<Header title={"Home"} icon={<IconHome />} />
+      <NewPost />
+			{posts.map((post) => {
+				return <PostCard key={post.id} {...post} />;
+			})}
+		</Box>
+	);
+};
