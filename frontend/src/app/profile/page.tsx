37bee5ffd4b38880666d8de2e7c78f7d8d664e39
claustra01 <shinya430.g@gmail.com>
2024-11-15 02:01:44 +0900

    add: my profile page

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { MyTimeline } from "@/components/Timeline/MyTimeline";
+
+export default function MyProfilePage() {
+	return (
+		<main>
+			<div>
+				<MyTimeline />
+			</div>
+		</main>
+	);
+}
