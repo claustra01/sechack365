e814585212e4552b1f00852f58a5817437d9bae9
claustra01 <shinya430.g@gmail.com>
2024-11-27 23:52:52 +0900

    add: sqlx corresponding

--- api/pkg/model/post.go
+++ api/pkg/model/post.go
@@ -1,18 +1,18 @@
 package model
 
 type Post struct {
-	Id        string `json:"id"`
-	UserId    string `json:"user_id"`
-	Content   string `json:"content"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	Id        string `json:"id" db:"id"`
+	UserId    string `json:"user_id" db:"user_id"`
+	Content   string `json:"content" db:"content"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
 
 type PostWithUser struct {
-	Id        string `json:"id"`
-	UserId    string `json:"user_id"`
-	User      User   `json:"user"`
-	Content   string `json:"content"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	Id        string `json:"id" db:"id"`
+	UserId    string `json:"user_id" db:"user_id"`
+	User      User   `json:"user" db:"user"`
+	Content   string `json:"content" db:"content"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
--- api/pkg/model/user.go
+++ api/pkg/model/user.go
@@ -1,22 +1,22 @@
 package model
 
 type User struct {
-	Id             string `json:"id"`
-	Username       string `json:"username"`
-	Host           string `json:"host"`
-	Protocol       string `json:"protocol"`
-	HashedPassword string `json:"hashed_password"`
-	DisplayName    string `json:"display_name"`
-	Profile        string `json:"profile"`
-	Icon           string `json:"icon"`
-	CreatedAt      string `json:"created_at"`
-	UpdatedAt      string `json:"updated_at"`
+	Id             string `json:"id" db:"id"`
+	Username       string `json:"username" db:"username"`
+	Host           string `json:"host" db:"host"`
+	Protocol       string `json:"protocol" db:"protocol"`
+	HashedPassword string `json:"hashed_password" db:"hashed_password"`
+	DisplayName    string `json:"display_name" db:"display_name"`
+	Profile        string `json:"profile" db:"profile"`
+	Icon           string `json:"icon" db:"icon"`
+	CreatedAt      string `json:"created_at" db:"created_at"`
+	UpdatedAt      string `json:"updated_at" db:"updated_at"`
 }
 
 type ApUserIdentifier struct {
-	UserId     string `json:"user_id"`
-	PublicKey  string `json:"public_key"`
-	PrivateKey string `json:"private_key"`
-	CreatedAt  string `json:"created_at"`
-	UpdatedAt  string `json:"updated_at"`
+	UserId     string `json:"user_id" db:"user_id"`
+	PublicKey  string `json:"public_key" db:"public_key"`
+	PrivateKey string `json:"private_key" db:"private`
+	CreatedAt  string `json:"created_at" db:"created_at"`
+	UpdatedAt  string `json:"updated_at" db:"updated_at"`
 }
