92b6ac931aa7a0f2b4b7c2ff991cb7d9a8a1a57a
claustra01 <shinya430.g@gmail.com>
2024-11-28 22:58:52 +0900

    fix: delete post in mock reset

--- api/pkg/handler/dev.go
+++ api/pkg/handler/dev.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/claustra01/sechack365/pkg/framework"
 )
@@ -50,10 +51,16 @@ func GenerateMock(c *framework.Context) http.HandlerFunc {
 
 func ResetMock(c *framework.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		users, err := c.Controllers.User.FindAll()
 		if err != nil {
 			panic(err)
 		}
+		posts, err := c.Controllers.Post.FindTimeline(time.Now(), 10000)
+		if err != nil {
+			panic(err)
+		}
+
 		if len(users) == 0 {
 			if _, err := w.Write([]byte("Mock Data Not Found")); err != nil {
 				// NOTE: err should be nil
@@ -61,6 +68,13 @@ func ResetMock(c *framework.Context) http.HandlerFunc {
 			}
 			return
 		}
+
+		for _, post := range posts {
+			if err := c.Controllers.Post.Delete(post.Id); err != nil {
+				panic(err)
+			}
+		}
+
 		for _, user := range users {
 			if err := c.Controllers.ApUserIdentifier.DeleteById(user.Id); err != nil {
 				panic(err)
