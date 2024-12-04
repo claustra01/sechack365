daa82fd9758071b49ec33cc0db5884b8a895a29d
claustra01 <shinya430.g@gmail.com>
2024-12-04 22:46:45 +0900

    wip: disable follow handler

--- api/pkg/handler/follow.go
+++ api/pkg/handler/follow.go
@@ -1,115 +1,115 @@
 package handler
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
+// import (
+// 	"encoding/json"
+// 	"log"
+// 	"net/http"
 
-	"github.com/claustra01/sechack365/pkg/util"
+// 	"github.com/claustra01/sechack365/pkg/util"
 
-	"github.com/claustra01/sechack365/pkg/cerror"
-	"github.com/claustra01/sechack365/pkg/framework"
-	"github.com/claustra01/sechack365/pkg/model"
-)
+// 	"github.com/claustra01/sechack365/pkg/cerror"
+// 	"github.com/claustra01/sechack365/pkg/framework"
+// 	"github.com/claustra01/sechack365/pkg/model"
+// )
 
-type FollowRequestBody struct {
-	Follower string `json:"follower"`
-	Followee string `json:"followee"`
-}
+// type FollowRequestBody struct {
+// 	Follower string `json:"follower"`
+// 	Followee string `json:"followee"`
+// }
 
-func CreateFollow(c *framework.Context) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// parse request body
-		var followRequestBody FollowRequestBody
-		body := make([]byte, r.ContentLength)
-		if _, err := r.Body.Read(body); err != nil {
-			// NOTE: err should be nil
-			panic(err)
-		}
-		err := json.Unmarshal(body, &followRequestBody)
-		if err != nil {
-			returnBadRequest(w, c.Logger, err)
-			return
-		}
+// func CreateFollow(c *framework.Context) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		// parse request body
+// 		var followRequestBody FollowRequestBody
+// 		body := make([]byte, r.ContentLength)
+// 		if _, err := r.Body.Read(body); err != nil {
+// 			// NOTE: err should be nil
+// 			panic(err)
+// 		}
+// 		err := json.Unmarshal(body, &followRequestBody)
+// 		if err != nil {
+// 			returnBadRequest(w, c.Logger, err)
+// 			return
+// 		}
 
-		// check follower protocol
-		follower, err := c.Controllers.User.FindById(followRequestBody.Follower)
-		if err != nil {
-			returnInternalServerError(w, c.Logger, err)
-			return
-		}
-		if follower == nil {
-			returnNotFound(w, c.Logger, cerror.ErrUserNotFound)
-			return
-		}
-		if follower.Protocol != model.ProtocolLocal {
-			returnBadRequest(w, c.Logger, cerror.ErrInvalidFollowRequest)
-			return
-		}
+// 		// check follower protocol
+// 		follower, err := c.Controllers.User.FindById(followRequestBody.Follower)
+// 		if err != nil {
+// 			returnInternalServerError(w, c.Logger, err)
+// 			return
+// 		}
+// 		if follower == nil {
+// 			returnNotFound(w, c.Logger, cerror.ErrUserNotFound)
+// 			return
+// 		}
+// 		if follower.Protocol != model.ProtocolLocal {
+// 			returnBadRequest(w, c.Logger, cerror.ErrInvalidFollowRequest)
+// 			return
+// 		}
 
-		// check followee protocol
-		followee, err := c.Controllers.User.FindById(followRequestBody.Followee)
-		if err != nil {
-			returnInternalServerError(w, c.Logger, err)
-			return
-		}
-		if followee == nil {
-			returnNotFound(w, c.Logger, cerror.ErrUserNotFound)
-			return
-		}
-		if followee.Protocol != model.ProtocolLocal && followee.Protocol != model.ProtocolActivityPub {
-			returnBadRequest(w, c.Logger, cerror.ErrInvalidFollowRequest)
-			return
-		}
+// 		// check followee protocol
+// 		followee, err := c.Controllers.User.FindById(followRequestBody.Followee)
+// 		if err != nil {
+// 			returnInternalServerError(w, c.Logger, err)
+// 			return
+// 		}
+// 		if followee == nil {
+// 			returnNotFound(w, c.Logger, cerror.ErrUserNotFound)
+// 			return
+// 		}
+// 		if followee.Protocol != model.ProtocolLocal && followee.Protocol != model.ProtocolActivityPub {
+// 			returnBadRequest(w, c.Logger, cerror.ErrInvalidFollowRequest)
+// 			return
+// 		}
 
-		// create follow
-		follow, err := c.Controllers.Follow.Create(followRequestBody.Follower, followRequestBody.Followee)
-		if err != nil {
-			returnInternalServerError(w, c.Logger, err)
-			return
-		}
+// 		// create follow
+// 		follow, err := c.Controllers.Follow.Create(followRequestBody.Follower, followRequestBody.Followee)
+// 		if err != nil {
+// 			returnInternalServerError(w, c.Logger, err)
+// 			return
+// 		}
 
-		switch followee.Protocol {
-		case model.ProtocolLocal:
-			// nop
-		case model.ProtocolActivityPub:
-			// get signer key params
-			keyId := c.Controllers.ActivityPub.NewKeyIdUrl(follower.Host, follower.Username)
-			followerIdentifier, err := c.Controllers.ApUserIdentifier.FindById(follower.Id)
-			if err != nil {
-				returnInternalServerError(w, c.Logger, err)
-				return
-			}
-			privateKey, err := util.DecodePrivateKeyPem(followerIdentifier.PrivateKey)
-			if err != nil {
-				returnInternalServerError(w, c.Logger, err)
-				return
-			}
+// 		switch followee.Protocol {
+// 		case model.ProtocolLocal:
+// 			// nop
+// 		case model.ProtocolActivityPub:
+// 			// get signer key params
+// 			keyId := c.Controllers.ActivityPub.NewKeyIdUrl(follower.Host, follower.Username)
+// 			followerIdentifier, err := c.Controllers.ApUserIdentifier.FindById(follower.Id)
+// 			if err != nil {
+// 				returnInternalServerError(w, c.Logger, err)
+// 				return
+// 			}
+// 			privateKey, err := util.DecodePrivateKeyPem(followerIdentifier.PrivateKey)
+// 			if err != nil {
+// 				returnInternalServerError(w, c.Logger, err)
+// 				return
+// 			}
 
-			// get followee actor url
-			followeeUrl, err := c.Controllers.ActivityPub.ResolveWebfinger(followee.Username, followee.Host)
-			if err != nil {
-				returnInternalServerError(w, c.Logger, err)
-				return
-			}
+// 			// get followee actor url
+// 			followeeUrl, err := c.Controllers.ActivityPub.ResolveWebfinger(followee.Username, followee.Host)
+// 			if err != nil {
+// 				returnInternalServerError(w, c.Logger, err)
+// 				return
+// 			}
 
-			// send follow activity
-			followActivity := c.Controllers.ActivityPub.NewFollowActivity(follow.Id, follower.Host, follower.Id, followeeUrl)
-			followeeActor, err := c.Controllers.ActivityPub.ResolveRemoteActor(followActivity.Object)
-			if err != nil {
-				returnInternalServerError(w, c.Logger, err)
-				return
-			}
-			respBody, err := c.Controllers.ActivityPub.SendActivity(followeeActor.Inbox, followActivity, c.Config.Host, keyId, privateKey)
-			if err != nil {
-				c.Logger.Error("Remote follow error", "ERROR", string(respBody))
-				returnInternalServerError(w, c.Logger, err)
-				return
-			}
+// 			// send follow activity
+// 			followActivity := c.Controllers.ActivityPub.NewFollowActivity(follow.Id, follower.Host, follower.Id, followeeUrl)
+// 			followeeActor, err := c.Controllers.ActivityPub.ResolveRemoteActor(followActivity.Object)
+// 			if err != nil {
+// 				returnInternalServerError(w, c.Logger, err)
+// 				return
+// 			}
+// 			respBody, err := c.Controllers.ActivityPub.SendActivity(followeeActor.Inbox, followActivity, c.Config.Host, keyId, privateKey)
+// 			if err != nil {
+// 				c.Logger.Error("Remote follow error", "ERROR", string(respBody))
+// 				returnInternalServerError(w, c.Logger, err)
+// 				return
+// 			}
 
-			// FIXME: This is debug
-			log.Println(string(respBody))
-			return
-		}
-	}
-}
+// 			// FIXME: This is debug
+// 			log.Println(string(respBody))
+// 			return
+// 		}
+// 	}
+// }
