e168333e996603bf4be01005a6517022a77b93a5
claustra01 <shinya430.g@gmail.com>
2024-12-27 23:50:00 +0900

    fix: nodeinfo handler

--- api/pkg/handler/nodeinfo.go
+++ api/pkg/handler/nodeinfo.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/claustra01/sechack365/pkg/cerror"
 	"github.com/claustra01/sechack365/pkg/framework"
 )
 
@@ -10,9 +11,10 @@ func Nodeinfo2_0(c *framework.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		users, err := c.Controllers.User.FindAll()
 		if err != nil {
-			returnInternalServerError(w, c.Logger, err)
+			c.Logger.Error("Internal Server Error", "Error", cerror.Wrap(err, "failed to get nodeinfo"))
+			returnError(w, http.StatusInternalServerError)
 		}
 		nodeinfo := c.Controllers.ActivityPub.NewNodeInfo(len(users))
-		jsonResponse(w, nodeinfo)
+		returnResponse(w, http.StatusOK, ContentTypeJson, nodeinfo)
 	}
 }
