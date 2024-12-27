fbd8d9ea283c1375a88162f187dc2d034b5dca9f
claustra01 <shinya430.g@gmail.com>
2024-12-27 14:15:47 +0900

    fix: post repository

--- api/pkg/repository/post.go
+++ api/pkg/repository/post.go
@@ -13,30 +13,35 @@ type PostRepository struct {
 	SqlHandler model.ISqlHandler
 }
 
-func (r *PostRepository) Create(userId, content string) (*model.Post, error) {
-	post := new(model.Post)
+func (r *PostRepository) Create(userId, content string) error {
 	uuid := util.NewUuid()
 	query := `
 		INSERT INTO posts (id, user_id, content)
 		VALUES ($1, $2, $3)
 		RETURNING *;
 	`
-	if err := r.SqlHandler.Get(post, query, uuid, userId, content); err != nil {
-		return nil, err
+	if _, err := r.SqlHandler.Exec(query, uuid, userId, content); err != nil {
+		return err
 	}
-	return post, nil
+	return nil
 }
 
 func (r *PostRepository) FindById(id string) (*model.PostWithUser, error) {
 	post := new(model.PostWithUser)
 	query := `
 		SELECT posts.*,
-		users.username AS user_username,
+		CASE
+			WHEN users.protocol = 'local' THEN '@' || users.username
+			WHEN users.protocol = 'activitypub' THEN ap_user_identifiers.local_username || '@' || ap_user_identifiers.host
+			WHEN users.protocol = 'nostr' THEN nostr_user_identifiers.npub
+		END AS user_username,
 		users.protocol AS user_protocol,
 		users.display_name AS user_display_name,
-		users.profile AS user_profile,
 		users.icon AS user_icon
-		FROM posts JOIN users ON posts.user_id = users.id
+		FROM posts
+		JOIN users ON posts.user_id = users.id
+		LEFT JOIN ap_user_identifiers ON users.id = ap_user_identifiers.user_id
+		LEFT JOIN nostr_user_identifiers ON users.id = nostr_user_identifiers.user_id
 		WHERE posts.id = $1;
 	`
 	err := r.SqlHandler.Get(post, query, id)
@@ -53,12 +58,18 @@ func (r *PostRepository) FindTimeline(createdAt time.Time, limit int) ([]*model.
 	rawPosts := make([]model.PostWithUser, 0)
 	query := `
 		SELECT posts.*,
-		users.username AS "user.username",
-		users.protocol AS "user.protocol",
-		users.display_name AS "user.display_name",
-		users.profile AS "user.profile",
-		users.icon AS "user.icon"
-		FROM posts JOIN users ON posts.user_id = users.id
+		CASE
+			WHEN users.protocol = 'local' THEN '@' || users.username
+			WHEN users.protocol = 'activitypub' THEN ap_user_identifiers.local_username || '@' || ap_user_identifiers.host
+			WHEN users.protocol = 'nostr' THEN nostr_user_identifiers.npub
+		END AS user_username,
+		users.protocol AS user_protocol,
+		users.display_name AS user_display_name,
+		users.icon AS user_icon
+		FROM posts
+		JOIN users ON posts.user_id = users.id
+		LEFT JOIN ap_user_identifiers ON users.id = ap_user_identifiers.user_id
+		LEFT JOIN nostr_user_identifiers ON users.id = nostr_user_identifiers.user_id
 		WHERE posts.created_at < $1
 		ORDER BY posts.created_at DESC LIMIT $2;
 	`
@@ -76,12 +87,18 @@ func (r *PostRepository) FindUserTimeline(userId string, createdAt time.Time, li
 	rawPosts := make([]model.PostWithUser, 0)
 	query := `
 		SELECT posts.*,
-		users.username AS "user.username",
-		users.protocol AS "user.protocol",
-		users.display_name AS "user.display_name",
-		users.profile AS "user.profile",
-		users.icon AS "user.icon"
-		FROM posts JOIN users ON posts.user_id = users.id
+		CASE
+			WHEN users.protocol = 'local' THEN '@' || users.username
+			WHEN users.protocol = 'activitypub' THEN ap_user_identifiers.local_username || '@' || ap_user_identifiers.host
+			WHEN users.protocol = 'nostr' THEN nostr_user_identifiers.npub
+		END AS user_username,
+		users.protocol AS user_protocol,
+		users.display_name AS user_display_name,
+		users.icon AS user_icon
+		FROM posts
+		JOIN users ON posts.user_id = users.id
+		LEFT JOIN ap_user_identifiers ON users.id = ap_user_identifiers.user_id
+		LEFT JOIN nostr_user_identifiers ON users.id = nostr_user_identifiers.user_id
 		WHERE user_id = $1 AND posts.created_at < $2
 		ORDER BY posts.created_at DESC LIMIT $3;
 	`
