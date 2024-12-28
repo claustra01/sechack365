c0fb1e2b731372ff704913248153de364d965c48
claustra01 <shinya430.g@gmail.com>
2024-12-29 01:14:27 +0900

    fix: lint

--- frontend/src/styles/devices.tsx
+++ frontend/src/styles/devices.tsx
@@ -1,17 +1,9 @@
-import styles from './devices.module.css'
+import styles from "./devices.module.css";
 
 export const MobileOnly = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className={styles.mobileOnly}>
-      {children}
-    </div>
-  )
-}
+	return <div className={styles.mobileOnly}>{children}</div>;
+};
 
 export const DesktopOnly = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className={styles.desktopOnly}>
-      {children}
-    </div>
-  )
-}
+	return <div className={styles.desktopOnly}>{children}</div>;
+};
