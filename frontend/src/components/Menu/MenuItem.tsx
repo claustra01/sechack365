1fa0b593b4daa29923a8ef86e776474c9230a062
claustra01 <shinya430.g@gmail.com>
2024-12-29 01:37:07 +0900

    chore: link in menu text

--- frontend/src/components/Menu/MenuItem.tsx
+++ frontend/src/components/Menu/MenuItem.tsx
@@ -1,6 +1,5 @@
 import { colors } from "@/styles/colors";
-import { ActionIcon, Box, Title } from "@mantine/core";
-import Link from "next/link";
+import { ActionIcon, Anchor, Flex, Title } from "@mantine/core";
 
 export type MenuItemProps = {
 	icon: JSX.Element;
@@ -10,19 +9,15 @@ export type MenuItemProps = {
 
 export const MenuItem = (props: MenuItemProps) => {
 	return (
-		<Box style={{ display: "flex", alignItems: "center" }}>
-			<ActionIcon
-				component={Link}
-				href={props.href}
-				variant="subtle"
-				size="xl"
-				color={colors.secondaryColor}
-			>
-				{props.icon}
-			</ActionIcon>
-			<Title size="h3" fw={500} c={colors.secondaryColor}>
-				{props.title}
-			</Title>
-		</Box>
+		<Anchor href={props.href} style={{ textDecoration: "none" }}>
+			<Flex direction="row" align="center">
+				<ActionIcon variant="subtle" size="xl" color={colors.secondaryColor}>
+					{props.icon}
+				</ActionIcon>
+				<Title size="h3" fw={500} c={colors.secondaryColor}>
+					{props.title}
+				</Title>
+			</Flex>
+		</Anchor>
 	);
 };
