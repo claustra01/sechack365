1f54b875e509fc4a9c0b7d27c6d63581502cc8e1
claustra01 <shinya430.g@gmail.com>
2024-12-30 23:38:05 +0900

    update: view follow list

--- frontend/src/components/Timeline/UserTimeline.tsx
+++ frontend/src/components/Timeline/UserTimeline.tsx
@@ -1,7 +1,13 @@
-import { getApiV1LookupUsername, getApiV1UsersIdPosts } from "@/openapi/api";
+import {
+	getApiV1LookupUsername,
+	getApiV1UsersIdFollowers,
+	getApiV1UsersIdFollows,
+	getApiV1UsersIdPosts,
+} from "@/openapi/api";
 import type { Post, User } from "@/openapi/schemas";
 import { Box } from "@mantine/core";
 import { useEffect, useState } from "react";
+import { FollowList } from "../FollowList/FollowList";
 import { UserProfile } from "../Profile/UserProfile";
 import { TimelineBase } from "./TimelineBase";
 
@@ -10,8 +16,21 @@ export type UserTimelineProps = {
 };
 
 export const UserTimeline = (props: UserTimelineProps) => {
+	const [hash, setHash] = useState<string>("");
 	const [user, setUser] = useState<User | null>(null);
 	const [posts, setPosts] = useState<Post[]>([]);
+	const [follows, setFollows] = useState<User[]>([]);
+
+	useEffect(() => {
+		const handleHashChange = () => {
+			setHash(window.location.hash);
+		};
+		setHash(window.location.hash);
+		window.addEventListener("hashchange", handleHashChange);
+		return () => {
+			window.removeEventListener("hashchange", handleHashChange);
+		};
+	}, []);
 
 	useEffect(() => {
 		getApiV1LookupUsername(props.username).then((response) => {
@@ -27,6 +46,19 @@ export const UserTimeline = (props: UserTimelineProps) => {
 		}
 	}, [user?.id]);
 
+	useEffect(() => {
+		if (user?.id && hash === "#follows") {
+			getApiV1UsersIdFollows(user.id).then((response) => {
+				setFollows(response.data as unknown as User[]);
+			});
+		}
+		if (user?.id && hash === "#followers") {
+			getApiV1UsersIdFollowers(user.id).then((response) => {
+				setFollows(response.data as unknown as User[]);
+			});
+		}
+	}, [user?.id, hash]);
+
 	if (!user) {
 		return <Box>Loading...</Box>;
 	}
@@ -34,7 +66,11 @@ export const UserTimeline = (props: UserTimelineProps) => {
 	return (
 		<Box>
 			<UserProfile {...user} />
-			<TimelineBase posts={posts} />
+			{hash === "#follows" || hash === "#followers" ? (
+				<FollowList users={follows} />
+			) : (
+				<TimelineBase posts={posts} />
+			)}
 		</Box>
 	);
 };
