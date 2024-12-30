d59270888541b81e93a8984cd0c222b6ad43c648
claustra01 <shinya430.g@gmail.com>
2024-12-30 19:49:53 +0900

    update: bind username

--- frontend/utils/strings.ts
+++ frontend/utils/strings.ts
@@ -3,11 +3,11 @@ import { User } from "@/openapi/schemas";
 export const bindUsername = (props: User) => {
 	switch (props.protocol) {
 		case "local":
-			return `@${props.username}@${props.identifiers.activitypub?.host}\n${props.identifiers.nostr?.public_key}`;
+			return `@${props.username}@${props.identifiers.activitypub?.host}\n${props.identifiers.nostr?.npub}`;
 		case "activitypub":
 			return `@${props.identifiers.activitypub?.local_username}@${props.identifiers.activitypub?.host}`;
 		case "nostr":
-			return props.identifiers.nostr?.public_key;
+			return props.identifiers.nostr?.npub;
 		default:
 			return "";
 	}
