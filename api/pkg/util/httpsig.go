d878a4497c9d6f6cd76a578280fa29b16831ae16
claustra01 <shinya430.g@gmail.com>
2025-01-02 16:10:29 +0900

    wip: httpsig signer

--- api/pkg/util/httpsig.go
+++ api/pkg/util/httpsig.go
@@ -1,10 +1,16 @@
 package util
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/claustra01/sechack365/pkg/cerror"
 )
@@ -73,4 +79,57 @@ func GenerateKeyPemPair() (string, string, error) {
 		return "", "", cerror.Wrap(err, "failed to generate key pem pair")
 	}
 	return privKeyPem, pubKeyPem, nil
+}
+
+func SignRequest(keyname string, privKey *rsa.PrivateKey, req *http.Request, content []byte) error {
+	// headers
+	headers := []string{"(request-target)", "date", "host"}
+	if strings.ToLower(req.Method) != "get" {
+		headers = append(headers, "content-type", "digest")
+	}
+
+	// digest
+	var stuff []string
+	for _, h := range headers {
+		var s string
+		switch h {
+		case "(request-target)":
+			s = strings.ToLower(req.Method) + " " + req.URL.RequestURI()
+		case "date":
+			s = req.Header.Get(h)
+			if s == "" {
+				s = time.Now().UTC().Format(http.TimeFormat)
+				req.Header.Set(h, s)
+			}
+		case "host":
+			s = req.Header.Get(h)
+			if s == "" {
+				s = req.URL.Hostname()
+				req.Header.Set(h, s)
+			}
+		case "content-type":
+			s = req.Header.Get(h)
+		case "digest":
+			s = req.Header.Get(h)
+			if s == "" {
+				hash := sha256.New()
+				hash.Write(content)
+				s = "SHA-256=" + EncodeBase64(hash.Sum(nil))
+				req.Header.Set(h, s)
+			}
+		}
+		stuff = append(stuff, h+": "+s)
+	}
+
+	// signature
+	hash := sha256.New()
+	hash.Write([]byte(strings.Join(stuff, "\n")))
+	sig, err := rsa.SignPKCS1v15(rand.Reader, privKey, crypto.SHA256, hash.Sum(nil))
+	if err != nil {
+		return cerror.Wrap(err, "failed to sign request")
+	}
+	b64sig := EncodeBase64(sig)
+	sigHeader := fmt.Sprintf(`keyId="%s",algorithm="%s",headers="%s",signature="%s"`, keyname, "rsa-sha256", strings.Join(headers, " "), b64sig)
+	req.Header.Set("Signature", sigHeader)
+	return nil
 }
