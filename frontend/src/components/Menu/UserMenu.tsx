cbe8bd4ebbf682623f19c1c62a7745d2a6b565be
claustra01 <shinya430.g@gmail.com>
2025-01-03 19:38:45 +0900

    delete: current user profile

--- frontend/src/components/Menu/UserMenu.tsx
+++ frontend/src/components/Menu/UserMenu.tsx
@@ -1,8 +1,12 @@
 import { Box } from "@mantine/core";
 import { IconHome, IconLogout, IconUser } from "@tabler/icons-react";
+import { useContext } from "react";
+import { CurrentUserContext } from "../Template/PageTemplate";
 import { MenuItem, type MenuItemProps } from "./MenuItem";
 
 export const UserMenu = () => {
+	const { user } = useContext(CurrentUserContext);
+
 	const props: MenuItemProps[] = [
 		{
 			icon: <IconHome />,
@@ -12,7 +16,7 @@ export const UserMenu = () => {
 		{
 			icon: <IconUser />,
 			title: "My Profile",
-			href: "/profile",
+			href: `/profile/@${user?.username}`,
 		},
 		{
 			icon: <IconLogout />,
