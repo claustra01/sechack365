16ac2c8790e77f9ee43f976e8c3932dde2570502
claustra01 <shinya430.g@gmail.com>
2025-01-06 09:47:58 +0900

    fix: verify signature

--- api/pkg/handler/inbox.go
+++ api/pkg/handler/inbox.go
@@ -16,43 +16,43 @@ import (
 
 func ActorInbox(c *framework.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// // resolve public key
-		// sigHeader := r.Header.Get("Signature")
-		// re := regexp.MustCompile(`keyId="([^"]+)"`)
-		// match := re.FindStringSubmatch(sigHeader)
-		// if len(match) <= 1 {
-		// 	c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(cerror.ErrInvalidHttpSig, "failed to verify http signature"))
-		// 	returnError(w, http.StatusUnauthorized)
-		// 	return
-		// }
-		// keyId := match[1]
-		// actor, err := c.Controllers.ActivityPub.ResolveRemoteActor(keyId)
-		// if err != nil {
-		// 	c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(err, "failed to verify http signature"))
-		// 	returnError(w, http.StatusUnauthorized)
-		// 	return
-		// }
-		// pubKeyPem := actor.PublicKey.PublicKeyPem
+		// resolve public key
+		sigHeader := r.Header.Get("Signature")
+		re := regexp.MustCompile(`keyId="([^"]+)"`)
+		match := re.FindStringSubmatch(sigHeader)
+		if len(match) <= 1 {
+			c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(cerror.ErrInvalidHttpSig, "failed to verify http signature"))
+			returnError(w, http.StatusUnauthorized)
+			return
+		}
+		keyId := match[1]
+		actor, err := c.Controllers.ActivityPub.ResolveRemoteActor(keyId)
+		if err != nil {
+			c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(err, "failed to verify http signature"))
+			returnError(w, http.StatusUnauthorized)
+			return
+		}
+		pubKeyPem := actor.PublicKey.PublicKeyPem
 
-		// // verify signature
-		// _, pubKey, err := util.DecodePem(pubKeyPem)
-		// if err != nil {
-		// 	c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(err, "failed to verify http signature"))
-		// 	returnError(w, http.StatusUnauthorized)
-		// 	return
-		// }
+		// verify signature
+		_, pubKey, err := util.DecodePem(pubKeyPem)
+		if err != nil {
+			c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(err, "failed to verify http signature"))
+			returnError(w, http.StatusUnauthorized)
+			return
+		}
 		body := make([]byte, r.ContentLength)
 		if _, err := r.Body.Read(body); err != nil && err.Error() != "EOF" {
 			// NOTE: err should be nil
 			panic(err)
 		}
-		// _, err := util.HttpSigVerify(r, body, pubKey)
-		// // TODO: fix httpsig verifier
-		// if err != nil {
-		// 	c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(err, "failed to verify http signature"))
-		// 	// returnError(w, http.StatusUnauthorized)
-		// 	// return
-		// }
+		_, err = util.HttpSigVerify(r, body, pubKey)
+		// TODO: fix httpsig verifier
+		if err != nil {
+			c.Logger.Warn("Unauthorized", "Error", cerror.Wrap(err, "failed to verify http signature"))
+			// returnError(w, http.StatusUnauthorized)
+			// return
+		}
 
 		// parse activity
 		var activity map[string]interface{}
