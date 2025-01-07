74050fc2c0b5b5a410a782c906db7af37363cdad
claustra01 <shinya430.g@gmail.com>
2025-01-07 11:44:38 +0900

    fix: url href parser

--- frontend/utils/html.ts
+++ frontend/utils/html.ts
@@ -2,11 +2,12 @@ import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
 
 const parseUrlLink = (text: string) => {
-  const regex = /((https?):\/\/[^\s]+)/g;
+  const regex = /((https?):\/\/[\w!?/+\-_~;.,*&@#$%()'[\]]+)/g;
   return text.replace(regex, (url) => `<a href="${url}" target="_blank">${url}</a>`);
 }
 
 export const parseHtml = (html: string) => {
+  html = html.replaceAll("\n", "<br>");
   const sanitized =  DOMPurify.sanitize(html, {
     ALLOWED_TAGS: ["p", "span", "br"],
   });
