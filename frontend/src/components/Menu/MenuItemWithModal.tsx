679a2290cf172fdb3492ac781fbc2cd2a69d05d1
claustra01 <shinya430.g@gmail.com>
2025-01-21 13:40:07 +0900

    add: menuitem with modal

---
+++ frontend/src/components/Menu/MenuItemWithModal.tsx
@@ -0,0 +1,36 @@
+import { colors } from "@/styles/colors";
+import { Box, Button, Flex, Modal, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import React from "react";
+
+export type MenuItemWithModalProps = {
+	icon: JSX.Element;
+	title: string;
+	modalContent: JSX.Element;
+};
+
+export const MenuItemWithModal = (props: MenuItemWithModalProps) => {
+	const [opened, { open, close }] = useDisclosure(false);
+
+	return (
+		<Box p={4}>
+			<Modal opened={opened} onClose={close}>
+				{props.modalContent}
+			</Modal>
+
+			<Button
+				variant="subtle"
+				px={12}
+				onClick={open}
+				style={{ borderRadius: "16px" }}
+			>
+				<Flex direction="row" align="center">
+					{React.cloneElement(props.icon, { color: colors.secondaryColor })}
+					<Title size="h3" fw={500} c={colors.secondaryColor} ml="md">
+						{props.title}
+					</Title>
+				</Flex>
+			</Button>
+		</Box>
+	);
+};
