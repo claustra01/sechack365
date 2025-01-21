0d0e2df459aa63e1774609d1a259c957e09ace77
claustra01 <shinya430.g@gmail.com>
2025-01-21 14:37:08 +0900

    add: how to demo page

---
+++ frontend/src/app/how_to_demo/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { Header } from "@/components/Header/Header";
+import { PageTemplate } from "@/components/Template/PageTemplate";
+import { Box } from "@mantine/core";
+import { IconArrowBackUp } from "@tabler/icons-react";
+import Markdown from "react-markdown";
+
+export default function HowToDemoPage() {
+	const content = `
+# About
+このSNSはActivityPubとNostr双方のプロトコルに対応しています。
+
+ユーザーを作成して適当なリモートアカウントをフォローし、リモートアカウントから投稿することでこのサーバーのユーザーのタイムラインに表示されます。
+また、リモートユーザーからこのアカウントをフォローし、このアカウントから投稿することで、リモートユーザーのタイムラインに表示されます。
+
+# Note
+このデモサーバーはテスト用のため、データは定期的に削除されます。
+連合SNSの特性上、このサーバーとフォロー関係を作成するActivityPubやNostrのアカウントは使い捨てのものを推奨します。
+
+以下のインスタンスで使い捨てのアカウントが発行できます。リモートアカウントを作成する場合はこれらのインスタンスの使用を強く推奨します。
+- ActivityPub: [ActivityPub Academy](https://activitypub.academy/)
+- Nostr: [Nostter](https://nostter.app/)
+
+# Search
+フォローを行うには、ユーザーを連合から検索する必要があります。
+
+ユーザーIDはActivityPubの場合\`@user@domain\`の型、Nostrの場合\`npub1xxxxxx....\`の形をしています。
+このサーバーのユーザーのIDはログイン後のメニューから飛ぶことができるプロフィールページにどちらも表示されています。
+リモートインスタンス上でこれらを検索することで、このユーザーをリモートから検索・フォローすることが出来ます。
+
+リモートのアカウントをこのサーバーで検索する場合、検索フォームにユーザーIDを入力して検索してください。
+もし検索フォームがまだ実装されていない/見つからない場合は\`https://yojohan-demo.claustra01.net/profile/{ユーザーID}\`を開いてください。
+そこからそのリモートユーザーのフォローや投稿閲覧が可能です。
+  `;
+
+	return (
+		<PageTemplate>
+			<Header title={"How to demo"} icon={<IconArrowBackUp />} />
+      <Box p={24}>
+			<Markdown>{content}</Markdown>
+
+      </Box>
+		</PageTemplate>
+	);
+}
