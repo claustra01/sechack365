23442488ba0483addd6934f41e8bf09b40a9c66a
claustra01 <shinya430.g@gmail.com>
2025-01-21 13:40:16 +0900

    fix: lint

--- frontend/src/components/Menu/MenuItem.tsx
+++ frontend/src/components/Menu/MenuItem.tsx
@@ -14,19 +14,19 @@ export const MenuItem = (props: MenuItemProps) => {
 
 	return (
 		<Box p={4}>
-		<Button
-			variant="subtle"
-			px={12}
-			onClick={() => router.push(props.href)}
-			style={{ borderRadius: "16px" }}
-		>
-			<Flex direction="row" align="center">
-				{React.cloneElement(props.icon, { color: colors.secondaryColor })}
-				<Title size="h3" fw={500} c={colors.secondaryColor} ml="md">
-					{props.title}
-				</Title>
-			</Flex>
-		</Button>
+			<Button
+				variant="subtle"
+				px={12}
+				onClick={() => router.push(props.href)}
+				style={{ borderRadius: "16px" }}
+			>
+				<Flex direction="row" align="center">
+					{React.cloneElement(props.icon, { color: colors.secondaryColor })}
+					<Title size="h3" fw={500} c={colors.secondaryColor} ml="md">
+						{props.title}
+					</Title>
+				</Flex>
+			</Button>
 		</Box>
 	);
 };
