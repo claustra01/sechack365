11081add5e62be403c01ea6ce45ffff9bf2a6ffa
claustra01 <shinya430.g@gmail.com>
2025-01-23 06:44:56 +0900

    fix: username in response

--- api/pkg/repository/article.go
+++ api/pkg/repository/article.go
@@ -30,10 +30,18 @@ func (r *ArticleRepository) FindById(id string) (*model.ArticleWithUser, error) 
 		articles.created_at,
 		articles.updated_at,
 		users.id AS "user.id",
+		CASE
+			WHEN users.protocol = 'local' THEN '@' || users.username
+			WHEN users.protocol = 'activitypub' THEN '@' || ap_user_identifiers.local_username || '@' || ap_user_identifiers.host
+			WHEN users.protocol = 'nostr' THEN nostr_user_identifiers.npub
+		END AS "user.username",
+		users.protocol AS "user.protocol",
 		users.display_name AS "user.display_name",
 		users.icon AS "user.icon"
 		FROM articles
 		JOIN users ON articles.user_id = users.id
+		LEFT JOIN ap_user_identifiers ON users.id = ap_user_identifiers.user_id
+		LEFT JOIN nostr_user_identifiers ON users.id = nostr_user_identifiers.user_id
 		WHERE articles.id = $1;
 	`
 	if err := r.SqlHandler.Get(article, query, id); err != nil {
@@ -45,12 +53,24 @@ func (r *ArticleRepository) FindById(id string) (*model.ArticleWithUser, error) 
 func (r *ArticleRepository) FindCommentsByArticleId(articleId string) ([]*model.ArticleCommentWithUser, error) {
 	var comments []*model.ArticleCommentWithUser
 	query := `
-		SELECT article_comments.*,
+		SELECT
+		article_comments.id,
+		article_comments.content,
+		article_comments.created_at,
+		article_comments.updated_at,
 		users.id AS "user.id",
+		CASE
+			WHEN users.protocol = 'local' THEN '@' || users.username
+			WHEN users.protocol = 'activitypub' THEN '@' || ap_user_identifiers.local_username || '@' || ap_user_identifiers.host
+			WHEN users.protocol = 'nostr' THEN nostr_user_identifiers.npub
+		END AS "user.username",
+		users.protocol AS "user.protocol",
 		users.display_name AS "user.display_name",
 		users.icon AS "user.icon"
 		FROM article_comments
 		JOIN users ON article_comments.user_id = users.id
+		LEFT JOIN ap_user_identifiers ON users.id = ap_user_identifiers.user_id
+		LEFT JOIN nostr_user_identifiers ON users.id = nostr_user_identifiers.user_id
 		WHERE article_comments.article_id = $1
 		ORDER BY article_comments.created_at DESC;
 	`
