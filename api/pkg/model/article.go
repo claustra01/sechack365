16f77f4693e60b31ea10fe88a5bc19f5cf2f4025
claustra01 <shinya430.g@gmail.com>
2025-01-23 04:48:03 +0900

    add: article schemas

---
+++ api/pkg/model/article.go
@@ -0,0 +1,39 @@
+package model
+
+import "time"
+
+type Article struct {
+	Id        string    `db:"id"`
+	UserId    string    `db:"user_id"`
+	Title     string    `db:"title"`
+	Content   string    `db:"content"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+type ArticleComment struct {
+	Id        string    `db:"id"`
+	UserId    string    `db:"user_id"`
+	ArticleId string    `db:"article_id"`
+	Content   string    `db:"content"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+type ArticleWithUser struct {
+	Id        string     `db:"id"`
+	Title     string     `db:"title"`
+	Content   string     `db:"content"`
+	User      SimpleUser `db:"user"`
+	CreatedAt time.Time  `db:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at"`
+}
+
+type ArticleCommentWithUser struct {
+	Id        string     `db:"id"`
+	ArticleId string     `db:"article_id"`
+	Content   string     `db:"content"`
+	User      SimpleUser `db:"user"`
+	CreatedAt time.Time  `db:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at"`
+}
