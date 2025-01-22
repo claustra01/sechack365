ee994e313d14592dc05a750e8a863e1a466a36fb
claustra01 <shinya430.g@gmail.com>
2025-01-22 11:08:55 +0900

    update: header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { colors } from "@/styles/colors";
+import { DesktopOnly, MobileOnly } from "@/styles/devices";
 import { ActionIcon, Flex, Title } from "@mantine/core";
 import Link from "next/link";
 
@@ -9,25 +10,51 @@ export type HeaderProps = {
 
 export const Header = (props: HeaderProps) => {
 	return (
-		<Flex
-			bg={colors.secondaryColor}
-			align="center"
-			w="100%"
-			p={12}
-			style={{ overflow: "hidden" }}
-		>
-			<ActionIcon
-				component={Link}
-				href="/"
-				variant="subtle"
-				size="xl"
-				c={colors.primaryColor}
-			>
-				{props.icon}
-			</ActionIcon>
-			<Title size="h3" fw={500} c={colors.primaryColor}>
-				{props.title}
-			</Title>
-		</Flex>
+		<>
+			<DesktopOnly>
+				<Flex
+					bg={colors.secondaryColor}
+					align="center"
+					w="100%"
+					p="xs"
+					style={{ overflow: "hidden", borderRadius: "12px 12px 0 0" }}
+				>
+					<ActionIcon
+						component={Link}
+						href="/"
+						variant="subtle"
+						size="lg"
+						c={colors.primaryColor}
+					>
+						{props.icon}
+					</ActionIcon>
+					<Title size="h4" fw={500} c={colors.primaryColor}>
+						{props.title}
+					</Title>
+				</Flex>
+			</DesktopOnly>
+			<MobileOnly>
+				<Flex
+					bg={colors.secondaryColor}
+					align="center"
+					w="100%"
+					p="xs"
+					style={{ overflow: "hidden" }}
+				>
+					<ActionIcon
+						component={Link}
+						href="/"
+						variant="subtle"
+						size="lg"
+						c={colors.primaryColor}
+					>
+						{props.icon}
+					</ActionIcon>
+					<Title size="h4" fw={500} c={colors.primaryColor}>
+						{props.title}
+					</Title>
+				</Flex>
+			</MobileOnly>
+		</>
 	);
 };
