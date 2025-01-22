58d6b2f56306f220bf6a1eba22932665d940c661
claustra01 <shinya430.g@gmail.com>
2025-01-23 05:21:08 +0900

    add: article usecase

---
+++ api/pkg/usecase/article.go
@@ -0,0 +1,25 @@
+package usecase
+
+import "github.com/claustra01/sechack365/pkg/model"
+
+type IArticleRepository interface {
+	Create(id, userId, title, content string) error
+	FindById(id string) (*model.ArticleWithUser, error)
+	FindCommentsByArticleId(articleId string) ([]*model.ArticleCommentWithUser, error)
+}
+
+type ArticleUsecase struct {
+	ArticleRepository IArticleRepository
+}
+
+func (u *ArticleUsecase) Create(id, userId, title, content string) error {
+	return u.ArticleRepository.Create(id, userId, title, content)
+}
+
+func (u *ArticleUsecase) FindById(id string) (*model.ArticleWithUser, error) {
+	return u.ArticleRepository.FindById(id)
+}
+
+func (u *ArticleUsecase) FindCommentsByArticleId(articleId string) ([]*model.ArticleCommentWithUser, error) {
+	return u.ArticleRepository.FindCommentsByArticleId(articleId)
+}
