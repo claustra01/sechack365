5849c401292adb1a68de50731f299655f26b8684
claustra01 <shinya430.g@gmail.com>
2025-01-23 05:21:13 +0900

    add: article repository

---
+++ api/pkg/repository/article.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+	"github.com/claustra01/sechack365/pkg/cerror"
+	"github.com/claustra01/sechack365/pkg/model"
+)
+
+type ArticleRepository struct {
+	SqlHandler model.ISqlHandler
+}
+
+func (r *ArticleRepository) Create(id, userId, title, content string) error {
+	query := `
+		INSERT INTO articles (id, user_id, title, content)
+		VALUES ($1, $2, $3, $4);
+	`
+	if _, err := r.SqlHandler.Exec(query, id, userId, title, content); err != nil {
+		return cerror.Wrap(err, "failed to create article")
+	}
+	return nil
+}
+
+func (r *ArticleRepository) FindById(id string) (*model.ArticleWithUser, error) {
+	article := new(model.ArticleWithUser)
+	query := `
+		SELECT articles.*,
+		users.id AS "user.id",
+		users.display_name AS "user.display_name",
+		users.icon AS "user.icon"
+		FROM articles
+		JOIN users ON articles.user_id = users.id
+		WHERE articles.id = $1;
+	`
+	if err := r.SqlHandler.Get(article, query, id); err != nil {
+		return nil, cerror.Wrap(err, "failed to get article by id")
+	}
+	return article, nil
+}
+
+func (r *ArticleRepository) FindCommentsByArticleId(articleId string) ([]*model.ArticleCommentWithUser, error) {
+	var comments []*model.ArticleCommentWithUser
+	query := `
+		SELECT article_comments.*,
+		users.id AS "user.id",
+		users.display_name AS "user.display_name",
+		users.icon AS "user.icon"
+		FROM article_comments
+		JOIN users ON article_comments.user_id = users.id
+		WHERE article_comments.article_id = $1
+		ORDER BY article_comments.created_at DESC;
+	`
+	if err := r.SqlHandler.Select(&comments, query, articleId); err != nil {
+		return nil, cerror.Wrap(err, "failed to get comments by article id")
+	}
+	return comments, nil
+}
