6b969c50e6ff10af7f617c57d19701f9fd5ec5cc
claustra01 <shinya430.g@gmail.com>
2025-01-23 05:21:22 +0900

    add: article controller

---
+++ api/pkg/controller/article.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"github.com/claustra01/sechack365/pkg/model"
+	"github.com/claustra01/sechack365/pkg/repository"
+	"github.com/claustra01/sechack365/pkg/usecase"
+)
+
+type ArticleController struct {
+	ArticleUsecase usecase.ArticleUsecase
+}
+
+func NewArticleController(conn model.ISqlHandler) *ArticleController {
+	return &ArticleController{
+		ArticleUsecase: usecase.ArticleUsecase{
+			ArticleRepository: &repository.ArticleRepository{
+				SqlHandler: conn,
+			},
+		},
+	}
+}
+
+func (c *ArticleController) Create(id, userId, title, content string) error {
+	return c.ArticleUsecase.Create(id, userId, title, content)
+}
+
+func (c *ArticleController) FindById(id string) (*model.ArticleWithUser, error) {
+	return c.ArticleUsecase.FindById(id)
+}
+
+func (c *ArticleController) FindCommentsByArticleId(articleId string) ([]*model.ArticleCommentWithUser, error) {
+	return c.ArticleUsecase.FindCommentsByArticleId(articleId)
+}
