c8a7f3caf32a21ff71ffa5d80c0e85e1de705888
claustra01 <shinya430.g@gmail.com>
2025-10-07 22:46:37 +0900

    fix: webpack build cache warning

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack(config, { dev }) {
+    if (!dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import AxiosProvider from "./AxiosProvider";
-
-const _inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
